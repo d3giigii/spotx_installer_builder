78359b8db15feb41ba7a3d4906e8c4c6e5550386
d3giigii <lhammond997@gmail.com>
2023-10-02 16:03:22 -0400

    added gui

--- main.py
+++ main.py
@@ -70,16 +70,45 @@ binary_params = [
 ]
 
 # Language options. 
-languages = ["English", "French"]
+languages = {
+    "bn": "Bengali",
+    "de": "German",
+    "el": "Greek",
+    "en": "English",
+    "es": "Spanish",
+    "fa": "Persian",
+    "fi": "Finnish",
+    "fil": "Filipino",
+    "fr": "French",
+    "hi": "Hindi",
+    "hu": "Hungarian",
+    "id": "Indonesian",
+    "it": "Italian",
+    "ja": "Japanese",
+    "ka": "Georgian",
+    "ko": "Korean",
+    "lv": "Latvian",
+    "pl": "Polish",
+    "pt": "Portuguese",
+    "ro": "Romanian",
+    "ru": "Russian",
+    "sr": "Serbian",
+    "sv": "Swedish",
+    "tr": "Turkish",
+    "vi": "Vietnamese",
+    "zh": "Chinese",
+}
 
-# Tk setup. 
+### Tk
+## Tk setup. 
 window = tk.Tk()
 
-# Tk vars. 
+## Tk vars. 
 version_to_gen = ""
 language_choice = tk.StringVar(window)
-language_choice.set(languages[0])
+language_choice.set("English")
 
+## Tk widgets. 
 # Install section. 
 install_header_lbl = tk.Label(text="Install", font=("Arial", 16))
 version_lbl = tk.Label(text="Version")
@@ -87,7 +116,6 @@ version_ent = tk.Entry(textvariable=version_to_gen, width=20)
 csro_ckb = tk.Checkbutton(text="confirm_spoti_recomended_over")
 csru_ckb = tk.Checkbutton(text="confirm_spoti_recomended_uninstall")
 cums_ckb = tk.Checkbutton(text="confirm_uninstall_ms_spoti")
-
 # Experimental section. 
 experimental_header_lbl = tk.Label(text="Experimental", font=("Arial",16))
 exp_spotify_ckb = tk.Checkbutton(text="exp_spotify")
@@ -102,19 +130,35 @@ old_lyrics_ckb = tk.Checkbutton(text="old_lyrics")
 lyrics_stat_ckb = tk.Checkbutton(text="lyrics_stat")
 adsections_off_ckb = tk.Checkbutton(text="adsections_off")
 funnyprogressBar_ckb = tk.Checkbutton(text="funnyprogressBar")
-
 # Language section. 
 language_header_lbl = tk.Label(text="Language", font=("Arial", 16))
 language_lbl = tk.Label(text="Language")
-language_opm = tk.OptionMenu(window, language_choice, *languages)
-
-
-
+language_opm = tk.OptionMenu(window, language_choice, *[str(i) for i in languages.values()])
+# Podcast section. 
+podcast_header_lbl = tk.Label(text="Podcast", font=("Arial",16))
+podcasts_off_ckb = tk.Checkbutton(text="podcasts_off")
+podcasts_on_ckb = tk.Checkbutton(text="podcasts_on")
+# Update section. 
+Update_header_lbl = tk.Label(text="Update", font=("Arial",16))
+block_update_on_ckb = tk.Checkbutton(text="block_update_on")
+block_update_off_ckb = tk.Checkbutton(text="block_update_off")
+# Other section. 
+other_header_lbl = tk.Label(text="Other", font=("Arial", 25))
+cache_limit_ckb = tk.Checkbutton(text="Cache Limit")
+premium_ckb = tk.Checkbutton(text="premium")
+urlform_goofy_ckb = tk.Checkbutton(text="urlform_goofy")
+start_spoti_ckb = tk.Checkbutton(text="start_spoti")
+no_shortcut_ckb = tk.Checkbutton(text="no_shortcut")
 # Generate section. 
 generate_header_lbl = tk.Label(text="Generate", font=("Arial", 16))
 generate_btn = tk.Button(text="Generate Installer")
 
-
+# Return start of base batch command. 
+def get_base_start():
+    a = "%SYSTEMROOT%\System32\WindowsPowerShell\\v1.0\powershell.exe -Command "
+    b = '"&{[Net.ServicePointManager]::SecurityProtocol = 3072}; "'
+    c = '"""&{$(Invoke-WebRequest -UseBasicParsing \'https://spotx-official.github.io/run.ps1\')}'
+    return f"{a}{b}{c}"
 
 # Return end of base batch command. 
 def get_base_end():
@@ -135,6 +179,31 @@ def main():
     csru_ckb.grid(row=rows, column=0, sticky="W")
     rows += 1
     cums_ckb.grid(row=rows, column=0, sticky="W")
+
+    
+    
+    # Language section. 
+    rows += 1
+    language_header_lbl.grid(row=rows, column=0)
+    rows += 1
+    language_lbl.grid(row=rows, column=0, sticky="W")
+    language_opm.grid(row=rows, column=0)
+
+    # Podcast section. 
+    rows += 1
+    podcast_header_lbl.grid(row=rows, column=0)
+    rows += 1
+    podcasts_off_ckb.grid(row=rows, column=0, sticky="W")
+    rows += 1
+    podcasts_on_ckb.grid(row=rows, column=0, sticky="W")
+
+    # Update section. 
+    rows += 1
+    Update_header_lbl.grid(row=rows, column=0)
+    rows += 1
+    block_update_on_ckb.grid(row=rows, column=0, sticky="W")
+    rows += 1
+    block_update_off_ckb.grid(row=rows, column=0, sticky="W") 
 
     # Experimental section. 
     rows += 1
@@ -163,20 +232,20 @@ def main():
     adsections_off_ckb.grid(row=rows, column=0, sticky="W")
     rows += 1
     funnyprogressBar_ckb.grid(row=rows, column=0, sticky="W")
-    
-    # Language section. 
+
+    # Other section. 
     rows += 1
-    language_header_lbl.grid(row=rows, column=0)
+    other_header_lbl.grid(row=rows,column=0)
     rows += 1
-    language_lbl.grid(row=rows, column=0, sticky="W")
-    language_opm.grid(row=rows, column=0)
-
-    # Podcast section. 
-    
-
-    # TODO: Update section. 
-
-    # TODO: Other section. 
+    cache_limit_ckb.grid(row=rows,column=0, sticky="W")
+    rows += 1
+    premium_ckb.grid(row=rows,column=0, sticky="W")
+    rows += 1
+    urlform_goofy_ckb.grid(row=rows,column=0, sticky="W")
+    rows += 1
+    start_spoti_ckb.grid(row=rows,column=0, sticky="W")
+    rows += 1
+    no_shortcut_ckb.grid(row=rows,column=0, sticky="W")
 
     # Generate section. 
     rows += 1
@@ -185,13 +254,6 @@ def main():
     generate_btn.grid(row=rows, column=0, columnspan=2, ipadx=10)
 
     window.mainloop()
-
-# Return start of base batch command. 
-def get_base_start():
-    a = "%SYSTEMROOT%\System32\WindowsPowerShell\\v1.0\powershell.exe -Command "
-    b = '"&{[Net.ServicePointManager]::SecurityProtocol = 3072}; "'
-    c = '"""&{$(Invoke-WebRequest -UseBasicParsing \'https://spotx-official.github.io/run.ps1\')}'
-    return f"{a}{b}{c}"
 
 # Produce batch installer. 
 def generate_batch():
@@ -219,4 +281,3 @@ def write_out_command():
 
 if __name__ == "__main__":
     main()
-
